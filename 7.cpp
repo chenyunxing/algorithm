d72fc15d3c3eee99758152f09870b606e9655464
chenyunxing <807385017@qq.com>
2018-02-18 01:57:07 +0800

    2018-2-18 add Blue Bridge Cup previous questions Print binary tree horizontally(half right) 新增蓝桥杯 历届试题 横向打印二叉树(半对)

--- 7.cpp
+++ 7.cpp
@@ -1,13 +1,15 @@
 #include<iostream>
 #include<cstdlib>
 #include<cstring>
+#include<cstdio>
 using namespace std;
 struct two{
 	int num;
 	two *left;
 	two *right;
 };
-char a[100][10000];
+char a[100][2000000];
+int line=0;
 two *head;
 void add(two *now,int n){
 	if(n<now->num&&now->left==NULL){
@@ -35,39 +37,161 @@ void add(two *now,int n){
 	return;
 }
 void out(two *now,int i){
-	int n;
-	if(now->right!=NULL){
-		out(now->right,i+1);
+	int n,n2,temp1,temp2,temp3;
+	char temp;
+	//计算当前数字有多少位
+	temp2=now->num;
+	temp3=1;
+	temp1=now->num/10;
+	while(temp1!=0){
+		a[line][n]=temp2%10+'0';
+		temp2=temp2/10;
+		temp3++;
+		temp1=temp1/10;
 	}
-	for(n=0;n<i*2;n++){
-		cout<<".";
+
+	if(now->right!=NULL){
+		if(i==0){
+
+		}else{
+			temp3+=2;
+		}
+		if(now->right==NULL&&now->left==NULL){
+
+		}else{
+			temp3+=2;
+		}
+		out(now->right,i+temp3-1);
+	}
+	for(n=0;n<i;n++){
+		a[line][n]='.';
 	}
 	if(i==0){
-		cout<<now->num;
+		temp2=now->num;
+		temp3=1;
+		temp1=now->num/10;
+		while(temp1!=0){
+			a[line][n]=temp2%10+'0';
+			temp2=temp2/10;
+			n++;
+			temp3++;
+			temp1=temp1/10;
+		}
+		a[line][n]=temp2%10+'0';
+		//将逆序数字转为正序
+		for(n2=0;n2<temp3/2;n2++){
+			temp=a[line][n-n2];
+			a[line][n-n2]=a[line][n-temp3+1+n2];
+			a[line][n-temp3+1+n2]=temp;
+		}
 	}else{
-		cout<<"|-"<<now->num;
+		a[line][n]='|';
+		n++;
+		a[line][n]='-';
+		n++;
+		temp2=now->num;
+		temp3=1;
+		temp1=now->num/10;
+		while(temp1!=0){
+			a[line][n]=temp2%10+'0';
+			temp2=temp2/10;
+			n++;
+			temp3++;
+			temp1=temp1/10;
+		}
+		a[line][n]=temp2%10+'0';
+		//将逆序数字转为正序
+		for(n2=0;n2<temp3/2;n2++){
+			temp=a[line][n-n2];
+			a[line][n-n2]=a[line][n-temp3+1+n2];
+			a[line][n-temp3+1+n2]=temp;
+		}
 	}
 	if(now->right==NULL&&now->left==NULL){
-		cout<<endl;
+//		cout<<endl;
 	}else{
-		cout<<"-|"<<endl;
+		n++;
+		a[line][n]='-';
+		n++;
+		a[line][n]='|';
+		n++;
+	}
+	line++;
+	//计算当前数字有多少位
+	temp2=now->num;
+	temp3=1;
+	temp1=now->num/10;
+	while(temp1!=0){
+		a[line][n]=temp2%10+'0';
+		temp2=temp2/10;
+		temp3++;
+		temp1=temp1/10;
 	}
 	if(now->left!=NULL){
-		out(now->left,i+1);
+		if(i==0){
+
+		}else{
+			temp3+=2;
+		}
+		if(now->right==NULL&&now->left==NULL){
+
+		}else{
+			temp3+=2;
+		}
+		out(now->left,i+temp3-1);
 	}
 }
 int main(){
-	int n;
+	int n,sum=0,i,f;
 	head=(struct two *)malloc(sizeof(head));
 	cin>>n;
+	sum++;
 	head->left=NULL;
 	head->right=NULL;
 	head->num=n;
-	while(cin>>n,n!=-1){
-		
+	while(scanf("%d",&n)!=EOF){
+		sum++;
 		add(head,n);
 //		cout<<n<<endl;
 	}
-	
 	out(head,0);
+	for(n=0;n<sum;n++){
+		for(i=0;i<strlen(a[n]);i++){
+			if((a[n][i]=='|'&&a[n][i+1]=='-')||(a[n][i]=='-'&&a[n][i+1]=='|')){
+				for(f=0;f<sum;f++){
+					if(a[f][i]=='|'){
+						break;
+					}
+				}
+				if(f<n){
+					for(f=f+1;f<n;f++){
+						if(a[f][i]=='.'){
+							a[f][i]='|';
+						}
+					}
+				}
+				for(f=n+1;f<sum;f++){
+					if(a[f][i]=='|'){
+						break;
+					}
+				}
+				if(f!=sum){
+					for(f=f-1;f>n;f--){
+						if(a[f][i]=='.'){
+							a[f][i]='|';
+						}
+					}
+				}
+			}
+		}
+	}
+	if(sum==1){
+		cout<<a[n]<<"-|";
+		return 0;
+	}
+	for(n=0;n<sum;n++){
+		cout<<a[n]<<endl;
+	}
+
+	return 0;
 }
